66ee30f580208d4a85750995fa8ef3c093084e86
Alexandra <103836628+abaivel@users.noreply.github.com>
2023-02-03 23:21:44 +0100

    Add files via upload

---
+++ programmes_c/fichier.h
@@ -0,0 +1,85 @@
+#define TAILLE_MAX_LIGNE 200
+#define TAILLE_MAX_CASE 50
+
+typedef struct box {
+    char** tab;
+    int nb_colonnes;
+    double somme1;
+	double somme2;
+    int nb_times;
+    double minimum;
+    double maximum;
+}Case;
+
+typedef struct arbreAVL{
+	Case elmt;
+	struct arbreAVL* gauche;
+	struct arbreAVL* droit;
+	int equilibre;
+}ArbreAVL;
+
+typedef ArbreAVL* pArbreAVL;
+
+typedef struct arbre{
+	Case elmt;
+	struct arbre* gauche;
+	struct arbre* droit;
+}Arbre;
+
+typedef Arbre* pArbre;
+
+typedef struct chainon {
+    Case elmt;
+    struct chainon* suivant;
+}Chainon;
+
+//fichier fonctions.c
+Case creerCase(char** tab, int nb_colonnes, int colonne_moy1, int colonne_moy2);
+Chainon* entrer_donnees_liste(FILE* fichier_entree, FILE* fichier_sortie, int colonne_tri, int colonne_moy1, int colonne_moy2, int nb_colonnes, int moy1, int moy2, int mini, int maxi, int reverse, int date, int tri_multiple, int colonnes_tri_mutiple[3]);
+Arbre* entrer_donnees_abr(FILE* fichier_entree, FILE* fichier_sortie, int colonne_tri, int colonne_moy1, int colonne_moy2, int nb_colonnes, int moy1, int moy2, int mini, int maxi, int date, int tri_multiple, int colonnes_tri_mutiple[3]);
+ArbreAVL* entrer_donnees_avl(FILE* fichier_entree, FILE* fichier_sortie, int colonne_tri, int colonne_moy1, int colonne_moy2, int nb_colonnes, int moy1, int moy2, int mini, int maxi, int date, int tri_multiple, int colonnes_tri_mutiple[3]);
+char** LigneVersTab(char* ligne, int nb_colonnes);
+void libererTab2(char** tab, int t);
+void libererCase(Case c, int nb_colonnes);
+
+//fichier fonctions_liste.c
+Chainon* creationChainon(Case a);
+Chainon* insertionFin(Chainon* pliste, Chainon* c);
+Chainon* triInsertionMaxDecroissant(Chainon* liste);
+Chainon* ajout_croissant3(Chainon* pliste, char** tab, int nb_colonnes, int colonne_tri1, int colonne_tri2, int colonne_tri3);
+Chainon* ajout_croissant(Chainon* pliste, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy1, int colonne_moy2, int moyenne1, int moyenne2, int minimum, int maximum);
+Chainon* ajout_decroissant(Chainon* pliste, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy1, int colonne_moy2, int moyenne1, int moyenne2, int minimum, int maximum);
+Chainon* ajout_croissant_date(Chainon* pliste, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy, int moyenne);
+int ecrire_liste_in_file(FILE* fichier_sortie, Chainon* pliste, char type[4], int nb_colonnes);
+
+//fichier fonctions_abr.c
+pArbre creerArbre(Case r);
+pArbre insertionABR_date(pArbre a, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy, int moyenne);
+pArbre insertionABR(pArbre a, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy1, int colonne_moy2, int moyenne1, int moyenne2, int minimum, int maximum);
+pArbre insertionABR3(pArbre a, char** tab, int nb_colonnes, int colonne_tri1, int colonne_tri2, int colonne_tri3);
+pArbre insertionABR_avec_max(pArbre a, Case c);
+void triABR(pArbre arbre, pArbre* nouv_arbre,int nb_colonnes);
+int ecrire_abr_in_file_croissant(FILE* fichier_sortie, Arbre* arbre, char type[4], int* i, int nb_colonnes);
+int ecrire_abr_in_file_decroissant(FILE* fichier_sortie, Arbre* arbre, char type[4], int nb_colonnes);
+int ecrire_abr_in_file_p3t3(FILE* fichier_sortie, Arbre* arbre, char type[4], char heure[], char id[], int nb_colonnes);
+
+//fichier fonctions_avl.c
+pArbreAVL creerArbreAVL(Case r);
+int max2(int a, int b);
+int min2(int a, int b);
+int min3(int a, int b, int c);
+int max3(int a, int b, int c);
+pArbreAVL rotationGauche(pArbreAVL a);
+pArbreAVL rotationDroite(pArbreAVL a);
+pArbreAVL doubleRotationGauche(pArbreAVL a);
+pArbreAVL doubleRotationDroite(pArbreAVL a);
+pArbreAVL equilibrerAVL(pArbreAVL a);
+pArbreAVL insertionAVL(pArbreAVL a,int* h, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy1, int colonne_moy2, int moyenne1, int moyenne2, int minimum, int maximum);
+pArbreAVL insertionAVL_date(pArbreAVL a, int* h, char** tab, int nb_colonnes, int colonne_tri, int colonne_moy, int moyenne);
+pArbreAVL insertionAVL3(pArbreAVL a, int* h, char** tab, int nb_colonnes, int colonne_tri1, int colonne_tri2, int colonne_tri3);
+pArbreAVL insertionAVL_avec_max(pArbreAVL a, int* h, Case c);
+void triAVL(pArbreAVL arbre, pArbreAVL* nouv_arbre,int nb_colonnes);
+int ecrire_avl_in_file_croissant(FILE* fichier_sortie, ArbreAVL* arbre, char type[4], int* i, int nb_colonnes);
+int ecrire_avl_in_file_decroissant(FILE* fichier_sortie, ArbreAVL* arbre, char type[4], int nb_colonnes);
+int ecrire_avl_in_file_p3t3(FILE* fichier_sortie, ArbreAVL* arbre, char type[4], char heure[], char id[], int nb_colonnes);
+
---
+++ programmes_c/main.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fichier.h"
+
+int main(int argc, char **argv){
+	int reversed = 0;
+	int algo = 0;
+	char *nom_fichier_entre;
+	char *nom_fichier_sortie;
+	int num_colonne_sort=-1;
+	int num_colonne_moy=-1;
+	char type_donne[4]="";
+	char* fin_nom_fichier;
+	for (int i = 1; i < argc; i++){
+		if (strstr(argv[i], "-") != NULL){
+			if (!strcmp(argv[i], "-f") && i+1 < argc){
+				if (argv[i+1][0] != '-'){
+					nom_fichier_entre = argv[i+1];
+				}else{
+                    return 1;
+				}
+			} else if (!strcmp(argv[i], "-o") && i+1 < argc){
+				if (argv[i+1][0] != '-'){
+					nom_fichier_sortie = argv[i+1];
+				}else{
+                    return 1;
+				}
+            }else if (!strcmp(argv[i], "--avl")){
+				algo = 0;
+			} else if (!strcmp(argv[i], "--abr")){
+				algo = 1;
+			} else if (!strcmp(argv[i], "--tab")){
+				algo = 2;
+			}else if (!strcmp(argv[i], "-r")){
+				reversed = 1;
+			}/*else if (strstr(argv[i],"-t") !=NULL || strstr(argv[i],"-p") != NULL || !strcmp(argv[i],"-w") || !strcmp(argv[i],"-h") || !strcmp(argv[i],"-m")){
+                strcpy(type_donne,argv[i]);
+			}*/
+			else {
+				return 1;
+			}
+		}
+	}
+	fin_nom_fichier=strchr(nom_fichier_entre,'_');
+	int m=1;
+	type_donne[0]='-';
+	while (fin_nom_fichier[m]!='.' && m<strlen(fin_nom_fichier)){
+        type_donne[m]=fin_nom_fichier[m];
+        m++;
+	}
+	type_donne[m]='\0';
+	if (nom_fichier_entre == NULL || nom_fichier_sortie == NULL || strlen(type_donne) <= 1){
+		return 1;
+	}
+	FILE* fichier_entree=fopen(nom_fichier_entre,"r");
+	if (fichier_entree==NULL){
+        	return 2;
+	}
+	FILE* fichier_sortie=fopen(nom_fichier_sortie,"w");
+	if (fichier_sortie==NULL){
+        	return 3;
+	}
+	int nb_colonnes;
+	int r=0;
+	if (algo==0){
+		ArbreAVL* arbre;
+		if (!strcmp(type_donne,"-t1") || !strcmp(type_donne,"-p1")){
+		    arbre= entrer_donnees_avl(fichier_entree,fichier_sortie,0,1,-1,2,1,0,1,1,0,0,NULL);
+		    nb_colonnes=2;
+		    int i=0;
+		    r=ecrire_avl_in_file_croissant(fichier_sortie,arbre,type_donne,&i,2);
+		}else if (!strcmp(type_donne,"-t2") || !strcmp(type_donne,"-p2")){
+		    arbre= entrer_donnees_avl(fichier_entree,fichier_sortie,0,1,-1,2,1,0,0,0, 1,0,NULL);
+		    nb_colonnes=2;
+		    int i=0;
+		    r=ecrire_avl_in_file_croissant(fichier_sortie,arbre,type_donne,&i,2);
+		}else if (!strcmp(type_donne,"-t3") || !strcmp(type_donne,"-p3")){
+		    char heure[TAILLE_MAX_CASE]="";
+		    char id[TAILLE_MAX_CASE]="";
+		    int colonnes_tri[3]={2,0,1};
+		    arbre=entrer_donnees_avl(fichier_entree,fichier_sortie,-1,-1,-1,4,0,0,0,0,0,1,colonnes_tri);
+		    nb_colonnes=4;
+		    r=ecrire_avl_in_file_p3t3(fichier_sortie,arbre,type_donne,heure,id,4);
+		}else if (!strcmp(type_donne,"-w")){
+		    arbre= entrer_donnees_avl(fichier_entree,fichier_sortie,0,1,2,4,1,1,0,0,0,0,NULL);
+		    nb_colonnes=4;
+		    int i=0;
+		    r=ecrire_avl_in_file_croissant(fichier_sortie,arbre,type_donne,&i,4);
+		}else if (!strcmp(type_donne,"-h")){
+		    arbre= entrer_donnees_avl(fichier_entree,fichier_sortie,2,2,-1,3,0,0,0,0,0,0,NULL);
+		    nb_colonnes=3;
+		    int i=0;
+		    r=ecrire_avl_in_file_decroissant(fichier_sortie,arbre,type_donne,3);
+		}else if (!strcmp(type_donne,"-m")){
+		    int i=0;
+		    arbre=entrer_donnees_avl(fichier_entree,fichier_sortie,0,1,-1,4,0,0,0,1,0,0,NULL);
+		    ArbreAVL* nouv_arbre=NULL;
+		    triAVL(arbre, &nouv_arbre,4);
+		    nb_colonnes=4;
+		    r=ecrire_avl_in_file_decroissant(fichier_sortie,nouv_arbre,type_donne,4);
+		}
+		if (r==1){
+            return 3;
+		}
+	}else if (algo==1){
+		Arbre* arbre;
+		if (!strcmp(type_donne,"-t1") || !strcmp(type_donne,"-p1")){
+		    arbre= entrer_donnees_abr(fichier_entree,fichier_sortie,0,1,-1,2,1,0,1,1,0,0,NULL);
+		    nb_colonnes=2;
+		    int i=0;
+		    r=ecrire_abr_in_file_croissant(fichier_sortie,arbre,type_donne,&i,2);
+		}else if (!strcmp(type_donne,"-t2") || !strcmp(type_donne,"-p2")){
+		    arbre= entrer_donnees_abr(fichier_entree,fichier_sortie,0,1,-1,2,1,0,0,0, 1,0,NULL);
+		    nb_colonnes=2;
+		    int i=0;
+		    r=ecrire_abr_in_file_croissant(fichier_sortie,arbre,type_donne,&i,2);
+		}else if (!strcmp(type_donne,"-t3") || !strcmp(type_donne,"-p3")){
+		    char heure[TAILLE_MAX_CASE]="";
+		    char id[TAILLE_MAX_CASE]="";
+		    int colonnes_tri[3]={2,0,1};
+		    arbre=entrer_donnees_abr(fichier_entree,fichier_sortie,-1,-1,-1,4,0,0,0,0,0,1,colonnes_tri);
+		    nb_colonnes=4;
+		    r=ecrire_abr_in_file_p3t3(fichier_sortie,arbre,type_donne,heure,id,4);
+		}else if (!strcmp(type_donne,"-w")){
+		    arbre= entrer_donnees_abr(fichier_entree,fichier_sortie,0,1,2,4,1,1,0,0,0,0,NULL);
+		    nb_colonnes=4;
+		    int i=0;
+		    r=ecrire_abr_in_file_croissant(fichier_sortie,arbre,type_donne,&i,4);
+		}else if (!strcmp(type_donne,"-h")){
+		    arbre= entrer_donnees_abr(fichier_entree,fichier_sortie,2,2,-1,3,0,0,0,0,0,0,NULL);
+		    nb_colonnes=3;
+		    int i=0;
+		    r=ecrire_abr_in_file_decroissant(fichier_sortie,arbre,type_donne,3);
+		}else if (!strcmp(type_donne,"-m")){
+		    int i=0;
+		    arbre=entrer_donnees_abr(fichier_entree,fichier_sortie,0,1,-1,4,0,0,0,1,0,0,NULL);
+		    Arbre* nouv_arbre=NULL;
+		    triABR(arbre, &nouv_arbre,4);
+		    nb_colonnes=4;
+		    r=ecrire_abr_in_file_decroissant(fichier_sortie,nouv_arbre,type_donne,4);
+		}
+		if (r==1){
+            return 3;
+		}
+	}else if (algo==2){
+		Chainon* liste;
+		if (!strcmp(type_donne,"-t1") || !strcmp(type_donne,"-p1")){
+		    liste= entrer_donnees_liste(fichier_entree,fichier_sortie,0,1,-1,2,1,0,1,1,reversed,0,0,NULL);
+		    nb_colonnes=2;
+		}else if (!strcmp(type_donne,"-t2") || !strcmp(type_donne,"-p2")){
+		    liste= entrer_donnees_liste(fichier_entree,fichier_sortie,0,1,-1,2,1,0,0,0,reversed, 1,0,NULL);
+		    nb_colonnes=2;
+		}else if (!strcmp(type_donne,"-t3") || !strcmp(type_donne,"-p3")){
+		    int colonnes_tri[3]={2,0,1};
+		    liste=entrer_donnees_liste(fichier_entree,fichier_sortie,-1,-1,-1,4,0,0,0,0,reversed,0,1,colonnes_tri);
+		    nb_colonnes=4;
+		}else if (!strcmp(type_donne,"-w")){
+		    liste= entrer_donnees_liste(fichier_entree,fichier_sortie,0,1,2,4,1,1,0,0,reversed,0,0,NULL);
+		    nb_colonnes=4;
+		}else if (!strcmp(type_donne,"-h")){
+		    liste= entrer_donnees_liste(fichier_entree,fichier_sortie,2,2,-1,3,0,0,0,0,reversed,0,0,NULL);
+		    nb_colonnes=3;
+		}else if (!strcmp(type_donne,"-m")){
+		    liste=entrer_donnees_liste(fichier_entree,fichier_sortie,0,1,-1,4,0,0,0,1,reversed,0,0,NULL);
+		    liste=triInsertionMaxDecroissant(liste);
+		    nb_colonnes=4;
+		}
+		r=ecrire_liste_in_file(fichier_sortie,liste,type_donne,nb_colonnes);
+		if (r==1){
+            return 3;
+		}
+	}
+	fclose(fichier_entree);
+	fclose(fichier_sortie);
+	return 0;
+}
